df83571e7f051d1b19632fa4428dcc87673d00d5
jencely <jeny.guanuna@gmail.com>
2020-12-10 23:08:20 -0500

    deber 1 calculadora

---
+++ Deber1_Calculadora_estandar/Deber1_Calculadora_estandar/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Operacion;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//acciones a realizar
+            
+            Console.WriteLine("Que operación desea realizar");
+            Console.WriteLine("Para sumar [+]");
+            Console.WriteLine("Para restar [-]");
+            Console.WriteLine("Para multiplicar [*]");
+            Console.WriteLine("Para dividir [/]");
+            Console.WriteLine("Para potencia [^]");
+            Console.WriteLine("Para raiz [7]");
+            Console.Write("Elije una operación: ");
+
+            var opciones = Console.ReadLine();
+            //suma
+            if (opciones == "+")
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+
+                Console.WriteLine("Ingresar el segundo número");
+                var numero2 = Console.ReadLine();
+
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Sumar(int.Parse(numero1), int.Parse(numero2));
+
+                Console.WriteLine("La suma de los dos numeros es:   " + resultado);
+            }
+            //resta
+            if (opciones == "-") 
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+
+                Console.WriteLine("Ingresar el segundo número");
+                var numero2 = Console.ReadLine();
+
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Restar(int.Parse(numero1), int.Parse(numero2));
+
+                Console.WriteLine("La resta de los dos numeros es:   " + resultado);
+            }
+            //multiplicacion
+            if(opciones == "*")
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+
+                Console.WriteLine("Ingresar el segundo número");
+                var numero2 = Console.ReadLine();
+
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Multiplicar(int.Parse(numero1), int.Parse(numero2));
+
+                Console.WriteLine("La multiplicación de los dos numeros es:   " + resultado);
+            }
+            //división
+            if(opciones == "/")
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+
+                Console.WriteLine("Ingresar el segundo número");
+                var numero2 = Console.ReadLine();
+
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Dividir(int.Parse(numero1), int.Parse(numero2));
+
+                Console.WriteLine("La división de los dos numeros es:   " + resultado);
+            }
+            if(opciones == "^")
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+                
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Potencia(int.Parse(numero1));
+
+                Console.WriteLine("La potencia es:   " + resultado);
+            }
+            //raiz
+            if(opciones == "7")
+            {
+                Console.WriteLine("Ingresar el primer número");
+                var numero1 = Console.ReadLine();
+
+                var calculadora = new Matematica();
+
+                var resultado = calculadora.Raiz(int.Parse(numero1));
+
+                Console.WriteLine("La raiz es:   " + resultado);
+            }
+            Console.WriteLine("¿Deseas Continuar?");
+          
+            var respuesta = Console.ReadLine();
+           
+            Console.Clear();
+        }
+    }
+}
---
+++ Operacion/Operacion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operacion
+{
+    public class Matematica
+    {
+        public int Sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        public int Restar(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        public int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        public object Dividir(int numero1, int numero2)
+        {
+            if (numero2 != 0)
+            {
+                return numero1 / numero2;
+            }
+            else
+            {
+                return "No se puede dividir entre 0";
+            }
+        }
+        public int Potencia(int numero1)
+        {
+            return numero1 * numero1;
+        }
+
+        public int Raiz(int numero1)
+        {
+           return (int)Math.Sqrt(numero1);
+        }
+    }
+}
